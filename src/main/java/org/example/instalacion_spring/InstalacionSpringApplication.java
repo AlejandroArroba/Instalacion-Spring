9acc98316bf57525f3563c584677025ad244fda3
Alejandro Arroba <alejandroarroba2001@gmail.com>
2025-01-24 11:51:46 +0100

    Initial commit

---
+++ src/main/java/org/example/instalacion_spring/InstalacionSpringApplication.java
@@ -0,0 +1,13 @@
+package org.example.instalacion_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstalacionSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstalacionSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/instalacion_spring/InstalacionSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.instalacion_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstalacionSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
